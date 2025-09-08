f7801a2731a9b59627c9de40051af68db05ca463
Антон <wiggo20238@gmail.com>
2025-09-08 22:06:32 +0700

    l1.3

---
+++ l1/l1.3/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func startWorkers(ch chan int, n int, wg *sync.WaitGroup) { // n - количество воркеров
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go worker(ch, i, wg)
+	}
+}
+func worker(ch chan int, id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for v := range ch {
+		fmt.Printf("Воркер %v получил значение: %v\n", id, v)
+	}
+}
+
+func main() {
+	n := 10 // ИЗМЕНЕНИЕ КОЛ-ВА ВОРКЕРОВ
+	var wg sync.WaitGroup
+	ch := make(chan int)
+	startWorkers(ch, n, &wg)
+	// постоянная запись в канал
+	go func() {
+		i := 0
+		for {
+			ch <- i
+			i++
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
+	time.Sleep(10 * time.Second)
+	close(ch)
+	wg.Wait()
+}
